2473ddae345db8134dc377421fc55e782c00a54a
Yuvika <140506905+yuvika-09@users.noreply.github.com>
2025-09-25 19:46:06 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,25 +2,25 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const SECRET_KEY = 'mysecretkey';
 
-// Register
 exports.register = async (req, res) => {
     const { username, password } = req.body;
-    if (!username || !password) return res.status(400).json({ message: 'All fields required' });
-
+    if (!username || !password) {
+        return res.json({
+            message: 'All fields required' 
+        });
+    }
     const exists = await User.findOne({ username });
-    if (exists) return res.status(400).json({ message: 'User already exists' });
-
+    if (exists) return res.json({ message: 'User already exists' });
     const user = new User({ username, password });
     await user.save();
     res.json({ message: 'User registered successfully' });
 };
 
-// Login
+
 exports.login = async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username, password });
-    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
-
+    if (!user) return res.json({ message: 'Invalid credentials' });
     const token = jwt.sign({ id: user._id, username: user.username }, SECRET_KEY, { expiresIn: '1h' });
     res.json({ token });
 };
