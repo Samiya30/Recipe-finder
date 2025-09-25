923a8041add641ad957e7651b35e590bdc51689e
Yuvika <140506905+yuvika-09@users.noreply.github.com>
2025-09-25 15:42:31 +0530

    Update recipeController.js

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,17 +1,37 @@
-const recipes = []; 
-exports.getAllRecipes = (req, res) => {
+const Recipe = require('../models/Recipe');
+
+// Get all recipes of the logged-in user
+exports.getRecipes = async (req, res) => {
+    const recipes = await Recipe.find({ userId: req.user.id });
     res.json(recipes);
 };
 
-exports.addRecipe = (req, res) => {
-    const { title, ingredients, instructions } = req.body;
-    const recipe = { id: recipes.length + 1, title, ingredients, instructions, author: req.user.username };
-    recipes.push(recipe);
+// Add recipe
+exports.addRecipe = async (req, res) => {
+    const { name, ingredients, instructions, image } = req.body;
+    if (!name || !ingredients || !instructions) return res.status(400).json({ message: 'All fields required' });
+
+    const recipe = new Recipe({ userId: req.user.id, name, ingredients, instructions, image });
+    await recipe.save();
     res.json({ message: 'Recipe added', recipe });
 };
 
-exports.searchRecipes = (req, res) => {
-    const { query } = req.query;
-    const results = recipes.filter(r => r.title.toLowerCase().includes(query.toLowerCase()));
-    res.json(results);
+// Update recipe
+exports.updateRecipe = async (req, res) => {
+    const { id } = req.params;
+    const recipe = await Recipe.findOneAndUpdate(
+        { _id: id, userId: req.user.id },
+        req.body,
+        { new: true }
+    );
+    if (!recipe) return res.status(404).json({ message: 'Recipe not found' });
+    res.json({ message: 'Recipe updated', recipe });
+};
+
+// Delete recipe
+exports.deleteRecipe = async (req, res) => {
+    const { id } = req.params;
+    const recipe = await Recipe.findOneAndDelete({ _id: id, userId: req.user.id });
+    if (!recipe) return res.status(404).json({ message: 'Recipe not found' });
+    res.json({ message: 'Recipe deleted' });
 };
