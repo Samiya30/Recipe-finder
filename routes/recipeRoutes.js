abf8fc61e98e310128417c523b84b17354bcaa2a
Samiya30 <156989413+Samiya30@users.noreply.github.com>
2025-09-25 15:42:26 +0530

    Update recipeRoutes.js

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,33 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Recipe = require('../models/Recipe');
+const authMiddleware = require('../middleware/authMiddleware');
+const { getRecipes, addRecipe, updateRecipe, deleteRecipe } = require('../controllers/recipeController');
 
+router.use(authMiddleware);
 
-router.get('/', async (req, res) => {
-    const recipes = await Recipe.find();
-    res.json(recipes);
-});
-
-router.post('/', async (req, res) => {
-    const { name, ingredients, instructions, image } = req.body;
-    if(!name || !ingredients || !instructions) return res.json({ msg: 'Please enter all fields' });
-
-    const recipe = new Recipe({ name, ingredients, instructions, image });
-    await recipe.save();
-    res.json(recipe);
-});
-
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    await Recipe.findByIdAndDelete(id);
-    res.json({ msg: 'Recipe deleted' });
-});
-
-router.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    const { name, ingredients, instructions, image } = req.body;
-    const updated = await Recipe.findByIdAndUpdate(id, { name, ingredients, instructions, image }, { new: true });
-    res.json(updated);
-});
+router.get('/', getRecipes);
+router.post('/', addRecipe);
+router.put('/:id', updateRecipe);
+router.delete('/:id', deleteRecipe);
 
 module.exports = router;
