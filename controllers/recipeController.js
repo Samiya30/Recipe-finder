49814937fed06edc8783d6fab064e87b369c9e9d
Yuvika <140506905+yuvika-09@users.noreply.github.com>
2025-09-25 19:51:12 +0530

    Update recipeController.js

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,22 +1,33 @@
 const Recipe = require('../models/Recipe');
 
-// Get all recipes of the logged-in user
+
 exports.getRecipes = async (req, res) => {
     const recipes = await Recipe.find({ userId: req.user.id });
     res.json(recipes);
 };
 
-// Add recipe
+
 exports.addRecipe = async (req, res) => {
     const { name, ingredients, instructions, image } = req.body;
-    if (!name || !ingredients || !instructions) return res.status(400).json({ message: 'All fields required' });
-
-    const recipe = new Recipe({ userId: req.user.id, name, ingredients, instructions, image });
+    if (!name || !ingredients || !instructions) {
+        return res.json({ 
+            message: 'All fields required' 
+        });
+    }
+    const recipe = new Recipe({ 
+        userId: req.user.id, 
+        name, 
+        ingredients, 
+        instructions, 
+        image 
+    });
     await recipe.save();
-    res.json({ message: 'Recipe added', recipe });
+    res.json({ 
+        message: 'Recipe added', recipe 
+    });
 };
 
-// Update recipe
+
 exports.updateRecipe = async (req, res) => {
     const { id } = req.params;
     const recipe = await Recipe.findOneAndUpdate(
@@ -24,14 +35,15 @@ exports.updateRecipe = async (req, res) => {
         req.body,
         { new: true }
     );
-    if (!recipe) return res.status(404).json({ message: 'Recipe not found' });
+    if (!recipe) 
+        return res.json({ message: 'Recipe not found' });
     res.json({ message: 'Recipe updated', recipe });
 };
 
-// Delete recipe
+
 exports.deleteRecipe = async (req, res) => {
     const { id } = req.params;
     const recipe = await Recipe.findOneAndDelete({ _id: id, userId: req.user.id });
-    if (!recipe) return res.status(404).json({ message: 'Recipe not found' });
+    if (!recipe) return res.json({ message: 'Recipe not found' });
     res.json({ message: 'Recipe deleted' });
 };
