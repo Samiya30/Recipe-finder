72a76b3f5346bb8cbdc2809ec77f67a0da40d65e
upasana2515 <156989249+upasana2515@users.noreply.github.com>
2025-09-25 22:52:11 +0530

    Update Recipe.js

--- models/Recipe.js
+++ models/Recipe.js
@@ -5,7 +5,7 @@ const recipeSchema = new mongoose.Schema({
     name: { type: String, required: true },
     ingredients: { type: String, required: true },
     instructions: { type: String, required: true },
-    image: { type: String }
+    image: { type:String }
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
