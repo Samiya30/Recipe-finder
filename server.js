22462b6e7b99a204a98434570a820e0bfb8cf2f0
upasana2515 <156989249+upasana2515@users.noreply.github.com>
2025-09-25 22:47:37 +0530

    server created.js

--- server.js
+++ server.js
@@ -1,39 +1,32 @@
-require('dotenv').config();
-const connectDB = require('./config/db');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mongoose = require('mongoose');
-
 const authRoutes = require('./routes/authRoutes');
 const recipeRoutes = require('./routes/recipeRoutes');
 
 const app = express();
 const PORT = 3000;
 
-const mongoURI = 'mongodb://127.0.0.1:27017/recipeApp'; 
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/recipeDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/recipes', recipeRoutes);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
-// connect to MongoDB
-connectDB();
-
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
