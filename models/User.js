e457e241fefb3d1f030a369cfec49e50a8cbd585
upasana2515 <156989249+upasana2515@users.noreply.github.com>
2025-09-25 22:49:46 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true } // plain text for now
 });
 
 module.exports = mongoose.model('User', userSchema);
