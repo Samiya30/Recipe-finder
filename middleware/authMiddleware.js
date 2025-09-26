e1a62c2fbb12ca3ca49c0664dd68d832829e15fa
Samiya30 <156989413+Samiya30@users.noreply.github.com>
2025-09-26 14:33:05 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,14 +3,14 @@ const SECRET_KEY = 'mysecretkey';
 
 const authMiddleware = (req, res, next) => {
     const token = req.headers['authorization'];
-    if (!token) return res.status(401).json({ message: 'Access denied, token missing' });
+    if (!token) return res.json({ message: 'Access denied, token missing' });
 
     try {
         const decoded = jwt.verify(token, SECRET_KEY);
         req.user = decoded;
         next();
     } catch (err) {
-        res.status(401).json({ message: 'Invalid token' });
+        res.json({ message: 'Invalid token' });
     }
 };
 
